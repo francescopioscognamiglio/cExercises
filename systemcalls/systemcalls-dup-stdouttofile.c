352ef9732818c75d921dbdd365fa5cece90d537a
Francesco Pio Scognamiglio <francescopio.scognamiglio@4science.it>
2024-04-14 12:43:36 +0200

    fix error handling on dup system call

--- systemcalls/systemcalls-dup-stdouttofile.c
+++ systemcalls/systemcalls-dup-stdouttofile.c
@@ -6,24 +6,30 @@
 #define MESSAGE2 "How are you?\n"
 #define MESSAGE3 "Have you ever been to Rome?\n"
 
-void main() {
+int main() {
   int file = -1;
   if ((file = open("stdouttofile.test", O_WRONLY | O_CREAT)) <= -1) {
     printf("The file cannot be opened.\n");
+    return 1;
   }
 
   if (close(STDOUT_FILENO) <= -1) {
     printf("The standard output cannot be closed.\n");
+    return 1;
   }
 
   if (dup(file) != STDOUT_FILENO) {
     printf("The duplicated file is not the standard output.\n");
+    return 1;
   }
 
   if (close(file) <= -1) {
     printf("The file cannot be closed.\n");
+    return 1;
   }
 
   printf("This is a string redirect to the custom file.\n");
   printf("Have you ever seen a printf that write to a file?\n");
+
+  return 0;
 }
--- systemcalls/systemcalls-dup.c
+++ systemcalls/systemcalls-dup.c
@@ -6,22 +6,28 @@
 #define MESSAGE2 "How are you?\n"
 #define MESSAGE3 "Have you ever been to Rome?\n"
 
-void main() {
+int main() {
   // duplicate the standard output
   int fd = -1;
   if ((fd = dup(STDOUT_FILENO)) <= -1) {
     printf("The file cannot be duplicated.");
+    return 1;
   }
   printf("The duplicated file descriptor is %d\n", fd);
 
   // write to the standard output
   if (write(fd, MESSAGE1, sizeof(MESSAGE1)) <= -1) {
     printf("Error while writing into the file");
+    return 1;
   }
   if (write(fd, MESSAGE2, sizeof(MESSAGE2)) <= -1) {
     printf("Error while writing into the file");
+    return 1;
   }
   if (write(fd, MESSAGE3, sizeof(MESSAGE3)) <= -1) {
     printf("Error while writing into the file");
+    return 1;
   }
+
+  return 0;
 }
