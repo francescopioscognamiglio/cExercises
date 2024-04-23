f4fa0dbbe5c6f60359977325a5bd997cd2d55711
Francesco Pio Scognamiglio <francescopio.scognamiglio@4science.it>
2024-04-23 16:13:15 +0200

    pointers to structures

---
+++ structures/structures-pointers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+  float salary;
+  int identifier;
+} employee_t;
+
+int main() {
+  employee_t *emp = malloc(sizeof(employee_t));
+
+  // set values via dot operator
+  (*emp).salary = 10.5;
+  (*emp).identifier = 333;
+  printf("The employee has:\n\t-salary: %f\n\t-identifier: %d\n", (*emp).salary, (*emp).identifier);
+
+  // set values via arrow operator
+  emp->salary = 3.7;
+  emp->identifier = 567;
+    printf("The employee has:\n\t-salary: %f\n\t-identifier: %d\n", emp->salary, emp->identifier);
+}
