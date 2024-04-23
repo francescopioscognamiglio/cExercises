072cafb6da97605a6496e324e4468ed2c8d6308c
Francesco Pio Scognamiglio <francescopio.scognamiglio@4science.it>
2024-04-23 16:22:12 +0200

    normal structure and call by reference (pointers behind the scene)

---
+++ structures/structures-pointers-callbyreference2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+  float salary;
+  int identifier;
+} employee_t;
+
+void callByValue(employee_t emp) {
+  emp.salary = 35.3;
+  emp.identifier = 1111;
+}
+
+void callByReference(employee_t *emp) {
+  emp->salary = 22.7;
+  emp->identifier = 1232;
+}
+
+int main() {
+  employee_t emp = {2.0, 133};
+
+  callByValue(emp);
+  printf("The employee has:\n\t-salary: %f\n\t-identifier: %d\n", emp.salary, emp.identifier);
+
+  callByReference(&emp);
+  printf("The employee has:\n\t-salary: %f\n\t-identifier: %d\n", emp.salary, emp.identifier);
+}
