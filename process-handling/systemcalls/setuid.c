980d5801395c9219ffb11f4bac5fc2ab0ab4acec
Francesco Pio Scognamiglio <francescopio.scognamiglio@4science.it>
2024-05-16 16:53:08 +0200

    use setuid system call to set the ID of the user that executed the process

---
+++ process-handling/systemcalls/setuid.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int main() {
+  int oldUserId = getuid();
+  bool isUserChanged = setuid(55) == 0;
+  int newUserId = getuid();
+  if (!isUserChanged) {
+    perror("Failure while changing the user");
+    return EXIT_FAILURE;
+  }
+
+  printf("The ID of the user that executed this process was: %d, now is: %d\n", oldUserId, newUserId);
+  return EXIT_SUCCESS;
+}
