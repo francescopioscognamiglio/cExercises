fe4138442e12fd5bd8c98d61cc42d9cefd89e2b3
Francesco Pio Scognamiglio <francescopio.scognamiglio@4science.it>
2024-05-18 11:38:54 +0200

    use setenv system call to overwrite environment variables

--- environment-variables/setenv.c
+++ environment-variables/setenv.c
@@ -10,4 +10,14 @@ void main() {
 
   char *myCustomVar = getenv("my_custom_var");
   printf("The environment variable is my_custom_var=%s\n", myCustomVar);
+
+
+  // overwriting environment variable via setenv
+  status = setenv("my_custom_var", "my_custom_valXXX", 1);
+  if (status != 0) {
+    perror("Error while setting the environment variable");
+  }
+
+  myCustomVar = getenv("my_custom_var");
+  printf("The environment variable is my_custom_var=%s\n", myCustomVar);
 }
